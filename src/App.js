28058b04a95f4f4ca7b88c6eed3e1d7d4d5fdc28
yudai <sugi.nut@gmail.com>
2023-01-18 23:08:01 +0900

    :add: user recognition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports)
+
+
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          みいなちゅあん
-        </p>
+        {user ? (
+          <>
+            <h3>Hello! {user.username}</h3>
+            <button onClick={signOut}>Sign out</button>
+          </>
+        ) : (
+          <h3>Not authorized</h3>
+        )
+      }
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +35,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
